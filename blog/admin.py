62cbe40a541101a42345e1da03127452fa01e774
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-04-23 22:42:29 +0530

    define a blog post model and insert some data.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = [
+        field.name
+        for field in BlogPost._meta.get_fields()
+        if not (field.many_to_many or field.one_to_many)
+    ]
+    list_display_links = list_display
+    ordering = ("-id",)
+    list_filter = ["status"]
+    search_fields = ["title"]

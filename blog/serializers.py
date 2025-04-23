5f8171cc676b07307db1938760aada80b97e64bf
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-04-23 22:46:42 +0530

    feat: define blog post serializer.

---
+++ blog/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import BlogPost
+
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    """Serializer class for BlogPost"""
+
+    class Meta:
+        model = BlogPost
+        fields = ["id", "title", "slug", "owner", "content", "status", "created_at", "updated_at"]
+        read_only_fields = ["id", "slug", "owner", "created_at", "updated_at"]

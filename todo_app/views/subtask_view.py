4fdd7bfbab0ecce7daba9b699eb53f3ab0358d5a
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-18 11:45:24 +0530

    feat: define subtask crud opration.

--- todo_app/views/subtask_view.py
+++ todo_app/views/subtask_view.py
@@ -2,8 +2,10 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.throttling import UserRateThrottle
 from ..models.subtask_model import SubTask
 from ..serializers.subtask_serializer import SubTaskSerializer
+from rest_core.viewsets.mixins import ActionMessageMixin
 
-class SubTaskViewSet(ModelViewSet):
+
+class SubTaskViewSet(ActionMessageMixin, ModelViewSet):
     queryset = SubTask.objects.all()
     serializer_class = SubTaskSerializer
     throttle_classes = [UserRateThrottle]
